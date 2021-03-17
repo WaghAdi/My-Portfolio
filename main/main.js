a77be88cd5eae4cd51a97f094625f934dac59f23
aditya <adityawagh537>
2021-03-17 20:39:20 +0530

    basic disigen completed

--- main/main.js
+++ main/main.js
@@ -1,12 +1,40 @@
 var menuItems=document.getElementById("menuItems")
+
+
+
 menuItems.style.maxHeight="0px"
 function toggleMenu() {
   if ( menuItems.style.maxHeight=="0px") {
     menuItems.style.maxHeight="150px"
+    document.getElementById("MyElement").classList.remove('fa-bars');
+    document.getElementById("MyElement").classList.add('fa-times');
   } 
   else{
     menuItems.style.maxHeight="0px"
+    document.getElementById("MyElement").classList.remove('fa-times');
+    document.getElementById("MyElement").classList.add('fa-bars');
   }
+  
 
     
 }
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  center:true,
+  nav:true,
+  dots:false,
+  navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+})
